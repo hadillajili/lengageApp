d842bb4ad546a3e9aa7cfa869a0b32a1985eeac7
Hadil Lajili <300057135@fvtc.edu>
2021-05-31 05:49:43 +0200

    add multiple selection button

--- lib/views/screens/auth/login.dart
+++ lib/views/screens/auth/login.dart
@@ -16,7 +16,91 @@ import 'package:lengage_app/util/extensions.dart';
 /* to acess logged user info do 
 final auth = FirebaseAuth.instance;
 User user = auth.currentUser; */
+//*************************************** */
+class MultiSelectDialogItem<V> {
+  const MultiSelectDialogItem(this.value, this.label);
 
+  final V value;
+  final String label;
+}
+
+class MultiSelectDialog<V> extends StatefulWidget {
+  MultiSelectDialog({Key key, this.items, this.initialSelectedValues})
+      : super(key: key);
+
+  final List<MultiSelectDialogItem<V>> items;
+  final Set<V> initialSelectedValues;
+
+  @override
+  State<StatefulWidget> createState() => _MultiSelectDialogState<V>();
+}
+
+class _MultiSelectDialogState<V> extends State<MultiSelectDialog<V>> {
+  final _selectedValues = Set<V>();
+
+  void initState() {
+    super.initState();
+    if (widget.initialSelectedValues != null) {
+      _selectedValues.addAll(widget.initialSelectedValues);
+    }
+  }
+
+  void _onItemCheckedChange(V itemValue, bool checked) {
+    setState(() {
+      if (checked) {
+        _selectedValues.add(itemValue);
+      } else {
+        _selectedValues.remove(itemValue);
+      }
+    });
+  }
+
+  void _onCancelTap() {
+    Navigator.pop(context);
+  }
+
+  void _onSubmitTap() {
+    Navigator.pop(context, _selectedValues);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Select Country'),
+      contentPadding: EdgeInsets.only(top: 12.0),
+      content: SingleChildScrollView(
+        child: ListTileTheme(
+          contentPadding: EdgeInsets.fromLTRB(14.0, 0.0, 24.0, 0.0),
+          child: ListBody(
+            children: widget.items.map(_buildItem).toList(),
+          ),
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('CANCEL'),
+          onPressed: _onCancelTap,
+        ),
+        FlatButton(
+          child: Text('OK'),
+          onPressed: _onSubmitTap,
+        )
+      ],
+    );
+  }
+
+  Widget _buildItem(MultiSelectDialogItem<V> item) {
+    final checked = _selectedValues.contains(item.value);
+    return CheckboxListTile(
+      value: checked,
+      title: Text(item.label),
+      controlAffinity: ListTileControlAffinity.leading,
+      onChanged: (checked) => _onItemCheckedChange(item.value, checked),
+    );
+  }
+}
+
+//************************************************** */
 class Login extends StatefulWidget {
   @override
   _LoginState createState() => _LoginState();
@@ -27,12 +111,12 @@ class _LoginState extends State<Login> {
   bool validate = false;
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  String email="", password="", name = '';
+  String email = "", password = "", name = '';
   FocusNode nameFN = FocusNode();
   FocusNode emailFN = FocusNode();
   FocusNode passFN = FocusNode();
   FormMode formMode = FormMode.LOGIN;
-  
+
   final auth = FirebaseAuth.instance;
   DatabaseMethods db = new DatabaseMethods();
 
@@ -44,30 +128,27 @@ class _LoginState extends State<Login> {
       setState(() {});
       showInSnackBar('Please fix the errors in red before submitting.');
     } else {
-
-      if (formMode == FormMode.REGISTER){
-     
-        auth.createUserWithEmailAndPassword(email: email, password: password).then((value) async {
-          
-          Map<String,String> userInfoMap = {"name":name, "email": email};
+      if (formMode == FormMode.REGISTER) {
+        auth
+            .createUserWithEmailAndPassword(email: email, password: password)
+            .then((value) async {
+          Map<String, String> userInfoMap = {"name": name, "email": email};
           db.addUserInfo(userInfoMap); //adding user to firestore db
           User user = FirebaseAuth.instance.currentUser;
-          user.updateProfile(displayName:name); 
+          user.updateProfile(displayName: name);
           Navigate.pushPageReplacement(context, MainScreen());
-
-        } ).catchError((err){
+        }).catchError((err) {
           showInSnackBar('user already exists!');
         });
       }
-      if (formMode == FormMode.LOGIN){
-      
-        auth.signInWithEmailAndPassword(email: email, password: password)
-        .then((value){
+      if (formMode == FormMode.LOGIN) {
+        auth
+            .signInWithEmailAndPassword(email: email, password: password)
+            .then((value) {
           Navigate.pushPageReplacement(context, MainScreen());
-        } ).catchError((err){
+        }).catchError((err) {
           showInSnackBar('invalid credentials!');
         });
-        
       }
     }
   }
@@ -75,6 +156,31 @@ class _LoginState extends State<Login> {
   void showInSnackBar(String value) {
     _scaffoldKey.currentState.removeCurrentSnackBar();
     _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(value)));
+  }
+
+  void _showMultiSelect(BuildContext context) async {
+    ////multiItem = [];
+    // populateMultiselect();
+    //final items = multiItem;
+    final items = <MultiSelectDialogItem<int>>[
+      MultiSelectDialogItem(1, 'Fashion'),
+      MultiSelectDialogItem(2, 'IT&Multimedia'),
+      MultiSelectDialogItem(3, 'Decoration'),
+      MultiSelectDialogItem(4, 'Cars'),
+      MultiSelectDialogItem(5, 'Bags'),
+    ];
+
+    final selectedValues = await showDialog<Set<int>>(
+      context: context,
+      builder: (BuildContext context) {
+        return MultiSelectDialog(
+          items: items,
+        );
+      },
+    );
+
+    print(selectedValues);
+    // getvaluefromkey(selectedValues);
   }
 
   @override
@@ -246,6 +352,20 @@ class _LoginState extends State<Login> {
                   password = val;
                 },
                 focusNode: passFN,
+              ),
+              SizedBox(height: 20.0),
+              Visibility(
+                visible: formMode == FormMode.REGISTER,
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(primary: Colors.grey),
+                        child: Text(
+                            'Select your Interests                                             v'),
+                        onPressed: () => _showMultiSelect(context)),
+                    SizedBox(height: 20.0),
+                  ],
+                ),
               ),
             ],
           ),
